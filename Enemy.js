b42f256f75dac813c4eff39b28d95b36c4e043d2
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-07-02 18:46:33 +0200

    Enemy propieties

--- Enemy.js
+++ Enemy.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function Enemy(canvas) {
+  this.x = null;
+  this.y = null;
+  this.velocity = 6;
+  this.direction = null;
+  this.color = red;
+  this.width = 15;
+  this.height = 8;
+}
