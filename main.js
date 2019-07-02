fc8b58227f5fa6926d8bddbc0875ecb4acb0a0de
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-07-02 15:19:53 +0200

    created buildDom function and var mainElement

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 'use strict';
 
 function main() {
+  
+  var mainElement = document.querySelector('#site-main')
+
+  function buildDom(html) {
+    mainElement.innerHTML = html;
+    return mainElement;
+  };
 
 };
 
