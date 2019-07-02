65b40f77286727c5880a987c35b299cb6473aab1
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-07-02 16:28:42 +0200

    added Skydiver propieties

--- Skydiver.js
+++ Skydiver.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function Skydiver() {
+  this.canvas = canvas;
+  this.ctx = canvas.getContent('2d');
+  this.x = null;
+  this.y = null; 
+  this.direction = null;
+  this.color = 'blue';
+  this.width = 40;
+  this.height = 40;
+}
