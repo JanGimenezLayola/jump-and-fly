4f3fe17bf34e0655d953de4db1b2ded034d7950e
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-07-03 11:44:48 +0200

    SkyBackground created

--- SkyBackground.js
+++ SkyBackground.js
@@ -1,1 +1,13 @@
 'use strict';
+function Enemy(canvas) {
+  this.canvas = canvas;
+  this.ctx = this.canvas.getContext('2d');
+  this.x = null;
+  this.y = null;
+  this.velocity = 3;
+  this.skySlide = -7;
+  this.color = "red";
+  this.width = canvas.width;
+  this.height = canvas.height;
+}
+
